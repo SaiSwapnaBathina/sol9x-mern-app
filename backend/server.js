f31223c7c7d69089d0894a5573b2e577c69a76e1
SAI SWAPNA BATHINA <saiswapnabathina@SAIs-MacBook-Air.local>
2025-09-10 21:28:32 +0530

    updated the server.js file with cors for connecting backend to the frontend on vercel

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,4 @@
-// import express from 'express';
-// import cors from 'cors';
-// import connectDB from './config/db.js';
-// import { PORT } from './config/config.js';
 
-// import authRoutes from './routes/authRoutes.js';
-// import studentRoutes from './routes/studentRoutes.js';
-
-// const app = express();
-// connectDB();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/api/auth', authRoutes);
-// app.use('/api/students', studentRoutes);
-
-// // Base Route (Optional)
-// app.get('/', (req, res) => {
-//     res.send('🔗 SOL9X MERN Backend API is running...');
-//   });
-  
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server is running on port ${PORT}`);
-// });
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
@@ -35,7 +11,16 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+// app.use(cors()); --peevious
+
+//updated
+
+app.use(cors({
+  origin: "https://sol9x-mern-app.vercel.app", // your frontend URL
+  credentials: true,
+}));
+
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
