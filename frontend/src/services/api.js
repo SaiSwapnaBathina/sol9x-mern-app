761cc865e18a1a232567b95e57b8b27359b5e344
SAI SWAPNA BATHINA <saiswapnabathina@SAIs-MacBook-Air.local>
2025-09-10 22:59:39 +0530

    📝updation in services/api.js file and run command npm run build

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
-  // baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "/api"
 });
 
 api.interceptors.request.use((config) => {
